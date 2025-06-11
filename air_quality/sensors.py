305c11ab0d137953e03a26bb9deed58a2a7697b8
Lorn Jaeger <lornjaeger@proton.me>
2025-06-11 15:14:35 -0600

    reworked cams function

--- air_quality/sensors.py
+++ air_quality/sensors.py
@@ -47,6 +47,20 @@ def save(file):
 def try_auth():
     geemap.ee_initialize()
 
+def fmt_sensors(sensors):
+    sensors['Date GMT'] = pd.to_datetime(sensors['Date GMT'])
+    sensors['Time'] = sensors['Date GMT'] + pd.to_timedelta(sensors['Time GMT'] + ':00')
+    
+    sensors = sensors[["Time", "Latitude", "Longitude", "Sample Measurement"]]
+
+    return sensors
+
+def get_unique(sensors):
+    result = sensors.groupby(['Latitude', 'Longitude'])['Time'].agg(['min', 'max']).reset_index()
+    result.rename(columns={'min': 'Start Time', 'max': 'End Time'}, inplace=True)
+
+    return result
+
 
 def CONUS(sensors):
     sensors["CONUS"] = pd.NA
@@ -67,18 +81,72 @@ def AIRNOW(sensors):
     return sensors
 
 
+def parse_gee_region(raw):
+    headers = raw[0]
+    data = raw[1:]
+
+    df = pd.DataFrame(data, columns=headers)
+
+    df = df[df["time"].apply(lambda x: isinstance(x, (int, float)))]
+
+    df["Time"] = pd.to_datetime(df["time"], unit="ms")
+    df["Longitude"] = df["longitude"].astype(float)
+    df["Latitude"] = df["latitude"].astype(float)
+    df["pm25"] = df["particulate_matter_d_less_than_25_um_surface"].astype(float)
+
+    df["pm25"] *= 1_000_000_000
+
+    return df[["Time", "Longitude", "Latitude", "pm25"]]
+
 def CAMS(sensors):
     sensors["CAMS"] = pd.NA
+
+    min_start = pd.Timestamp("2016-06-22 12:00:00")
+    max_end = pd.Timestamp("2025-06-14 12:00:00")
+
+    cams = (
+        ImageCollection("ECMWF/CAMS/NRT")
+        .select("particulate_matter_d_less_than_25_um_surface")
+    )
+
+    cams_data = pd.DataFrame(columns=["Time", "Longitude", "Latitude", "pm25"]) #type: ignore
+
+    locations = get_unique(sensors)
+
+    # can use apply instead but this is only 1000 iterations
+    # most of the time is taken by api calls to earth engine
+    for _, row in locations.iterrows():
+        point = Geometry.Point(row["Longitude"], row["Latitude"])
+        # batching is needed so we don't hit the 3000 item limit
+        months = pd.date_range(pd.to_datetime(row["Start Time"]), pd.to_datetime(row["End Time"]), freq="ME")
+
+        for month in months:
+            start = month
+            end = month + pd.DateOffset(months=1)
+            monthly_data = (
+                cams
+                .filterBounds(point)
+                .filterDate(start, end)
+                .getRegion(point, scale=1)
+                .getInfo()
+            )
+            monthly_data = parse_gee_region(monthly_data)
+            # Needed overwrite. Google Earth Engine rounds lats and lons so there are slightly off
+            # Otherwise there will be no matches on merge
+            monthly_data["Latitude"] = row["Latitude"]
+            monthly_data["Longitude"] = row["Longitude"]
+            cams_data = pd.concat([cams_data, monthly_data], ignore_index=True) #type: ignore
+
+    sensors = sensors.merge(
+        cams_data,
+        on=["Time", "Latitude", "Longitude"],
+        how="left"
+    )
+
+    sensors["CAMS"] = sensors["pm25"]
+    sensors.drop(columns=["pm25"], inplace=True)
+
     return sensors
-
-def fmt_sensors(sensors):
-    sensors['Date GMT'] = pd.to_datetime(sensors['Date GMT'])
-    sensors['Time'] = sensors['Date GMT'] + pd.to_timedelta(sensors['Time GMT'] + ':00')
-    
-    sensors = sensors[["Time", "Latitude", "Longitude", "Sample Measurement"]]
-    return sensors
-
-
 
 def main() -> None:
     parse_args()
@@ -92,7 +160,10 @@ def main() -> None:
         sensors = pd.read_csv(file, low_memory=False)
         for source in sources:
             sensors = fmt_sensors(sensors)
-            sensors = source(sensors)
+            try:
+                sensors = source(sensors)
+            except Exception as e:
+                print(e)
             print(sensors)
         save(sensors)
 
