d1b1fc7c112f3fd56ff085379a9ef1c2dac1cd2e
Lorn Jaeger <lornjaeger@proton.me>
2025-07-18 12:30:01 -0600

    new scripts

---
+++ WRF/strip_wrf.py
@@ -0,0 +1,75 @@
+import xarray as xr
+import argparse
+from pathlib import Path
+import subprocess
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--wrf-path', type=Path, required=True)
+    args = parser.parse_args()
+
+    return args
+
+def get_directory_size(wrf_path):
+    result = subprocess.run(['du', '-sh', wrf_path], stdout=subprocess.PIPE, text=True)
+    return result.stdout.split()[0]
+
+def strip_wrfout(file):
+    ds = xr.open_dataset(file)
+
+    coords = ds.coords
+    attrs = ds.attrs
+
+    ds["Total Precipitation"] = ds["RAINNC"] + ds["RAINC"] 
+    ds["Specific Humidity"] = ds["Q2"] / (1 + ds["Q2"])
+
+    fields = [
+        "Total Precipitation",
+        "Specific Humidity",
+        "V10",
+        "U10",
+        "T2",
+        "XLAT",
+        "XLONG",
+    ]
+
+    ds = (
+            ds[fields]
+            .assign_coords(coords)
+            .assign_attrs(attrs)
+    )
+
+    return ds
+
+def write_wrf(ds):
+    pass
+    
+def main():
+    args = parse_args()
+    wrf_path = args.wrf_path
+
+    print(f"Starting Size: {get_directory_size(wrf_path)}") 
+
+    files = wrf_path.iterdir()
+    for file in files:
+        try:
+            ds = strip_wrfout(file)
+            write_wrf(ds)
+            print(f"Stripped {file.name}")
+        except Exception as e:
+            print(f"Error reading {file.name}: {e}")
+
+    print(f"Ending Size: {get_directory_size(wrf_path)}") 
+
+if __name__ == '__main__':
+    main()
+        
+
+
+
+
+   
+
+
+
+
